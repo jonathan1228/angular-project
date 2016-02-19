8f79d43bf69cc93a2a2dfb32e270ce097bc851be
Edward Tierra <edrichtierra@gmail.com>
2016-02-19 14:01:47 -0800

    fix head and add controllers.js

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+app.controller("HomeController", function($scope, $location){
+
+});
+app.controller("AboutController", function($scope, $location){
+
+});
+app.controller("SigninController", function($scope, $location){
+
+});
+app.controller("SignupController", function($scope, $location){
+
+});
